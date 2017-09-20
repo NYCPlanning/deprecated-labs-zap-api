b0003e3b903ba3253bafd7b395c9d704f4f5e9d2
Chris Whong <chris@whong.org>
2017-09-20 15:15:03 -0400

    only return active projects

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/ulurp/cd/:boroname/:cd.json', (req, res) => {
 
   const URL = 'http://a030-lucats.nyc.gov/lucats/ULURP_Search.aspx';
 
-  const activeProjects = rp({
+  rp({
     url: URL,
     jar: jar1,
     method: 'POST',
@@ -58,52 +58,17 @@ app.get('/ulurp/cd/:boroname/:cd.json', (req, res) => {
       __EVENTVALIDATION: aspHeaders.eventvalidation[boroAcronym],
       __VIEWSTATEGENERATOR: aspHeaders.viewstategenerator,
     },
-  });
+  })
+    .then((activeHTML) => {
+      const active = parse(activeHTML);
+      active.map((application) => {
+        const a = application;
+        a.status = 'active';
+        return a;
+      });
 
-  setTimeout(() => {
-    const completedProjects = rp({
-      url: URL,
-      jar: jar2,
-      method: 'POST',
-      followAllRedirects: true,
-      headers: {
-        Referer: 'http://a030-lucats.nyc.gov/lucats/ULURP_Search.aspx',
-      },
-      form: {
-        status: 'completed',
-        ddl_geography: 2,
-        TypeID: 0,
-        borough: boroAcronym,
-        cd,
-        Search2: 'search',
-        __VIEWSTATE: aspHeaders.viewstate[boroAcronym],
-        __EVENTVALIDATION: aspHeaders.eventvalidation[boroAcronym],
-        __VIEWSTATEGENERATOR: aspHeaders.viewstategenerator,
-      },
+      res.json(active);
     });
-
-    Promise.all([activeProjects, completedProjects])
-        .then((values) => {
-          const [activeHTML, completedHTML] = values;
-          const active = parse(activeHTML);
-          active.map((application) => {
-            const a = application;
-            a.status = 'active';
-            return a;
-          });
-
-          const completed = parse(completedHTML);
-          completed.map((application) => {
-            const a = application;
-            a.status = 'completed';
-            return a;
-          });
-
-          // combine active and completed into one array of objects
-          const response = active.concat(completed);
-          res.json(response);
-        });
-  }, 1100);
 });
 
 module.exports = app;
