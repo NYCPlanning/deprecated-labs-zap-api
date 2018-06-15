0d395e1652c14c3147d027ef429fe6f759f60462
allthesignals <wilburnforce@gmail.com>
2018-06-15 12:51:18 -0400

    Hotfix allow projects without param

--- routes/projects.js
+++ routes/projects.js
@@ -24,7 +24,7 @@ const listProjectsQuery = sql('../queries/projects/index.sql');
 const findProjectQuery = sql('../queries/projects/show.sql');
 
 /* GET /projects */
-router.get('/', async ({ query: { 'community-district': communityDistrict, page = 1, itemsPerPage = 30 } }, res) => {
+router.get('/', async ({ query: { 'community-district': communityDistrict = '', page = 1, itemsPerPage = 30 } }, res) => {
   try {
     const projects =
       await db.any(listProjectsQuery, { communityDistrict, itemsPerPage, offset: (page - 1) * itemsPerPage });
