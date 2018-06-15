70bb141044e729a2d5da49c65e7fc8608995ce7c
Chris Whong <chris@whong.org>
2018-06-14 23:59:13 -0400

    add filename to export api

--- routes/export.js
+++ routes/export.js
@@ -5,19 +5,19 @@ const db = pgp(process.env.DATABASE_CONNECTION_STRING);
 const router = express.Router();
 
 
-router.get('/projectbbls', (req, res) => {
+router.get('/projectbbls.json', (req, res) => {
   const SQL = `
     SELECT
       string_agg(dcp_bblnumber, ';') as bbls,
+      p.dcp_projectname,
       p.dcp_name as projectid
     FROM dcp_projectbbl b
     LEFT JOIN dcp_project p ON p.dcp_projectid = b.dcp_project
-    GROUP BY p.dcp_name;
+    GROUP BY p.dcp_name, p.dcp_projectname;
   `;
 
   db.any(SQL)
     .then((data) => {
-      console.log(data.length)
       res.send(data);
     })
     .catch((err) => {
