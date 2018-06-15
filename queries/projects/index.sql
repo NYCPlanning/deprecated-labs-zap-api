97015416a0be4c1719b0a60c1ae44391b720689a
allthesignals <wilburnforce@gmail.com>
2018-06-15 17:55:14 -0400

    sort descending

--- queries/projects/index.sql
+++ queries/projects/index.sql
@@ -2,8 +2,9 @@ SELECT
   dcp_name,
   dcp_projectname,
   dcp_projectbrief,
-  dcp_certifiedreferred
+  dcp_certifiedreferred,
+  dcp_projectid
 FROM dcp_project p
 WHERE dcp_validatedcommunitydistricts ILIKE '%${communityDistrict:value}%'
-ORDER BY dcp_name ASC
+ORDER BY dcp_name DESC
 LIMIT ${itemsPerPage:value} OFFSET ${offset:value}
