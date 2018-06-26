4e06c92e88fd752cb98b7f924c6c45ebeb7288a4
trbmcginnis <taylormcgin@gmail.com>
2018-06-25 20:08:51 -0400

    Changes to Standard Columns, add more attributes

--- routes/projects.js
+++ routes/projects.js
@@ -92,7 +92,14 @@ router.get('/', async (req, res) => {
     dcp_femafloodzoneshadedx,
     dcp_femafloodzonev,
     cast(count(dcp_projectid) OVER() as integer) as total_projects,
-    CASE WHEN c.geom IS NOT NULL THEN true ELSE false END AS has_centroid
+    CASE WHEN c.geom IS NOT NULL THEN true ELSE false END AS has_centroid,
+    (
+      SELECT ARRAY_AGG(a.dcp_ulurpnumber)
+      FROM dcp_projectaction a
+      WHERE a.dcp_project = p.dcp_projectid
+        AND a.statuscode <> 'Mistake'
+        AND a.dcp_ulurpnumber IS NOT NULL
+    ) AS ulurpnumbers
   `;
 
   const paginate = generatePaginate({ itemsPerPage, offset: (page - 1) * itemsPerPage });
