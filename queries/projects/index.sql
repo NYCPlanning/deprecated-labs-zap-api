e92412cec9f2e9d9dd546b01a94731deb487fbe4
Chris Whong <chris@whong.org>
2018-06-27 22:19:41 -0400

    get flood zone filters working again

--- queries/projects/index.sql
+++ queries/projects/index.sql
@@ -17,10 +17,10 @@ WHERE coalesce(dcp_publicstatus_simp, 'Unknown') IN (${dcp_publicstatus:csv})
   AND coalesce(dcp_ceqrtype, 'Unknown') IN (${dcp_ceqrtype:csv})
   AND coalesce(dcp_ulurp_nonulurp, 'Unknown') IN (${dcp_ulurp_nonulurp:csv})
   AND coalesce(dcp_ulurp_nonulurp, 'Unknown') IN (${dcp_ulurp_nonulurp:csv})
-  ${dcp_femafloodzoneaQuery^}
-  ${dcp_femafloodzonecoastalaQuery^}
-  ${dcp_femafloodzoneshadedxQuery^}
   ${dcp_femafloodzonevQuery^}
+  ${dcp_femafloodzonecoastalaQuery^}
+  ${dcp_femafloodzoneaQuery^}
+  ${dcp_femafloodzoneshadedxQuery^}
   AND dcp_visibility = 'General Public'
   ${communityDistrictsQuery^}
 ORDER BY dcp_name DESC
