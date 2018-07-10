22348ba0e7c33b51ce18c2a30d2d7a6d5ab50e08
Chris Whong <chris@whong.org>
2018-07-10 15:02:13 -0400

    Split projects route into multiple files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
+const NodeCache = require('node-cache');
+
 require('dotenv').config();
 
 const projects = require('./routes/projects');
@@ -7,6 +10,25 @@ const zap = require('./routes/zap');
 const exportRoute = require('./routes/export');
 
 const app = express();
+
+// log the SQL query
+const initOptions = {
+  // query(e) {
+  //    (process.env.DEBUG === 'true') ? console.log(e.query) : null; // eslint-disable-line
+  // },
+};
+
+const pgp = require('pg-promise')(initOptions);
+
+// initialize database connection
+app.db = pgp(process.env.DATABASE_CONNECTION_STRING);
+
+app.tileCache = new NodeCache({ stdTTL: 3600 });
+
+app.sql = (file) => {
+  const fullPath = path.join(__dirname, file);
+  return new pgp.QueryFile(fullPath, { minify: true });
+};
 
 // allows CORS
 app.all('*', (req, res, next) => {
---
+++ routes/projects/feedback.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Recaptcha } = require('express-recaptcha');
+const github = require('octonode');
+
+const router = express.Router();
+
+const client = github.client(process.env.GITHUB_ACCESS_TOKEN);
+const ghrepo = client.repo('NYCPlanning/zap-data-feedback');
+
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
+
+
+router.post('/', recaptcha.middleware.verify, async (req, res) => {
+  if (!req.recaptcha.error) {
+    // create a new issue
+    const { projectid, projectname, text } = req.body;
+    ghrepo.issue({
+      title: `Feedback about ${projectname}`,
+      body: `Project ID: [${projectid}](https://zap.planning.nyc.gov/projects/${projectid})\nFeedback: ${text}`,
+    }, () => {
+      res.send({
+        status: 'success',
+      });
+    });
+  } else {
+    res.status(403);
+    res.send({
+      status: 'captcha invalid',
+    });
+  }
+});
+
+module.exports = router;
