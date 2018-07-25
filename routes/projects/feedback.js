6ca6ca42a1524fb40070a80f2a8e4211aa9f1c3e
Chris Whong <chris@whong.org>
2018-07-25 11:45:02 -0400

    hotfix update reponame, better error handling

--- routes/projects/feedback.js
+++ routes/projects/feedback.js
@@ -5,7 +5,7 @@ const github = require('octonode');
 const router = express.Router();
 
 const client = github.client(process.env.GITHUB_ACCESS_TOKEN);
-const ghrepo = client.repo('NYCPlanning/zap-data-feedback');
+const ghrepo = client.repo('NYCPlanning/dcp-zap-data-feedback');
 
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
 
@@ -18,9 +18,15 @@ router.post('/', recaptcha.middleware.verify, async (req, res) => {
     ghrepo.issue({
       title: `Feedback about ${projectname}`,
       body: `Project ID: [${projectid}](https://zap.planning.nyc.gov/projects/${projectid})\nFeedback: ${text}`,
-    }, () => {
+    }, (err) => {
+      if (err) {
+        res.status(500);
+      } else {
+        res.status(201);
+      }
+
       res.send({
-        status: 'success',
+        status: err ? 'error' : 'success',
       });
     });
   } else {
