8aa3fa2ed86781014fe578f770d750f34420d6a2
Chris Whong <chris@whong.org>
2018-08-13 22:53:56 -0400

    refine response, build youtube link

--- utils/get-video-links.js
+++ utils/get-video-links.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const moment = require('moment');
 
 const api_key = process.env.AIRTABLE_API_KEY;
 
@@ -26,32 +27,50 @@ const getVideoLookup = (rows) => {
   return Promise.all(promises);
 };
 
+// main function, takes a projectid, returns a promise that resolves to an array of objects with youtube_link, date, and hearing_type
 const getVideoLinks = projectid => new Promise(async (resolve, reject) => {
   const api_call = `https://api.airtable.com/v0/app5fwvDYGjqdMv3B/project%20timestamps?maxRecords=10&view=Grid%20view&filterByFormula=(projectid = '${projectid}')&api_key=${api_key}`;
 
-  const rows = await fetch(api_call)
-    .then(d => d.json())
-    .then(({ records }) => records.map((record) => {
-      const { fields } = record;
-      const { hearing_video, video_timestamp } = fields;
+  try {
+    // get the timestamps and video ids associated with this projectid
+    const timestamps = await fetch(api_call)
+      .then(d => d.json())
+      .then(({ records }) => records.map((record) => {
+        const { fields } = record;
+        const { hearing_video, video_timestamp } = fields;
 
+        return {
+          hearing_video,
+          video_timestamp,
+        };
+      }));
+
+    // for each object in timestamps
+    const video_lookup = await getVideoLookup(timestamps);
+
+    const combined = timestamps.map(({ video_timestamp }, i) => {
+      const {
+        youtube_link,
+        date,
+        hearing_type,
+      } = video_lookup[i];
+
+      console.log(video_timestamp);
+
+      // concatenate the youtube link with the timestamp
+      const seconds = moment.duration(video_timestamp).asSeconds();
+      const video_link = `${youtube_link}&t=${seconds}s`;
       return {
-        hearing_video,
-        video_timestamp,
+        date,
+        hearing_type,
+        video_link,
       };
-    }));
+    });
 
-  const video_lookup = await getVideoLookup(rows);
-
-  const combined = rows.map(({ video_timestamp }, i) => {
-    const lookup = video_lookup[i];
-    return {
-      video_timestamp,
-      ...lookup,
-    };
-  });
-
-  resolve(combined);
+    resolve(combined);
+  } catch (e) {
+    reject(e);
+  }
 });
 
 module.exports = getVideoLinks;
