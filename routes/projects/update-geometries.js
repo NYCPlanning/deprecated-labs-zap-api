35c3751e5fb7c473315bc2722da6960c15da5cff
trbmcginnis <taylormcgin@gmail.com>
2019-01-16 12:56:47 -0500

    update comments

--- routes/projects/update-geometries.js
+++ routes/projects/update-geometries.js
@@ -11,6 +11,7 @@ router.get('/', async (req, res) => {
   const { id } = params;
 
   // import sql query templates
+  // SQL template to select list of bbls that match project id
   const matchBBLSQL = `
     SELECT
      (
@@ -24,6 +25,7 @@ router.get('/', async (req, res) => {
       AND dcp_visibility = 'General Public'
   `;
 
+  // SQL template, upsert command to insert rows that don't exist and update rows that do exist
   const upsertSQL = `
     INSERT INTO project_geoms(projectid, polygons, centroid)
     VALUES
@@ -36,11 +38,20 @@ router.get('/', async (req, res) => {
     DO NOTHING;
   `;
 
+  // SQL template to delete records that match the project id
+  // SELECT 1 returns a column of 1's for every row in the table
+  const deleteProjectSQL = `
+    DELETE FROM project_geoms WHERE EXISTS (
+      SELECT 1 FROM project_geoms WHERE projectid = \${id}
+    );
+  `;
+
   try {
-    if (!id.match(/^P?[0-9]{4}[A-Z]{1}[0-9]{4}$/)) throw new Error('Invalid project id');
-    const { bbls } = await app.db.one(matchBBLSQL, { id }); // destructuring
+    if (!id.match(/^P?[0-9]{4}[A-Z]{1}[0-9]{4}$/)) throw new Error('Invalid project id'); // regex match for project id with zero or one 'P', four numbers, 1 letter, and four numbers
+    const { bbls } = await app.db.one(matchBBLSQL, { id }); // an array of bbls that match the project id
+    // if a project has no bbls, remove project
     if (!bbls) {
-      app.db.none('DELETE FROM project_geoms WHERE projectid = \${id}', { id }) // eslint-disable-line
+      app.db.none(deleteProjectSQL, { id }) // eslint-disable-line,
         .then(() => {
           res.send({
             status: 'success',
@@ -48,7 +59,8 @@ router.get('/', async (req, res) => {
           });
         });
     } else {
-      const { polygons, centroid } = await getProjectGeoms(bbls); // check to see if this will fail in the right place (are we getting back bbls?), write a test for this
+      const { polygons, centroid } = await getProjectGeoms(bbls); // get geoms from carto that match array of bbls
+      // update geometry on existing project or insert new project with geoms (upsert)
       await app.db.none(upsertSQL, {
         id,
         polygons,
