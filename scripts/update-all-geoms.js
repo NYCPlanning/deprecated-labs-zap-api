36f63f8365bf8eac6e550df03c82a8e76bad97a4
Hannah Kates <hannah.b.kates@gmail.com>
2019-01-18 12:23:26 -0500

    eslint disable for one-off data processing script

--- scripts/update-all-geoms.js
+++ scripts/update-all-geoms.js
@@ -21,10 +21,10 @@ const getProjectsSQL = `
   const projects = await db.any(getProjectsSQL)
     .then(response => response.map(d => d.dcp_name))
     .catch((err) => {
-      console.log(err);
+      console.log(err); // eslint-disable-line
     });
 
-  console.log(`Found ${projects.length} projects`);
+  console.log(`Found ${projects.length} projects`); // eslint-disable-line
 
   // RUN API CALL FOR EACH PROJECT RECORD
 
@@ -34,12 +34,12 @@ const getProjectsSQL = `
     const status = await fetch(apiUrl)
       .then(d => d.json())
       .catch(() => {
-        console.log(`Something went wrong with ${projects[i]}`);
+        console.log(`Something went wrong with ${projects[i]}`); // eslint-disable-line
       });
-    if (status) console.log(`${i}: ${status.message}`);
+    if (status) console.log(`${i}: ${status.message}`); // eslint-disable-line
     i += 1;
     if (i === projects.length) {
-      console.log('Done');
+      console.log('Done'); // eslint-disable-line
     } else updateGeoms();
   };
 
