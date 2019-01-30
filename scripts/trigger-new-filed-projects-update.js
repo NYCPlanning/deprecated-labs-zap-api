db6ba35d3a252d7b0e144c541e6b09b4fcbdb558
jhogan <jhogan@planning.nyc.gov>
2019-01-30 17:05:47 -0500

    435: Add a retry to the api call in new-filed update script

--- scripts/trigger-new-filed-projects-update.js
+++ scripts/trigger-new-filed-projects-update.js
@@ -1,5 +1,5 @@
+const fetch = require('node-fetch');
 require('dotenv').config();
-const request = require('request-promise-native');
 
 const { SLACK_WEBHOOK_URL, HOST } = process.env;
 const argv = process.argv.slice(2);
@@ -10,38 +10,56 @@ const SlackWebhook = require('slack-webhook');
 const slack = new SlackWebhook(SLACK_WEBHOOK_URL);
 
 // set interval
-const interval_min = Number.isInteger(argv[0]) ? argv[0] : 60;
+const intervalMin = Number.isInteger(argv[0]) ? argv[0] : 60;
+const maxRetries = 3;
 
-slack.send(`Initializing newly filed project update task with ${interval_min} min interval`);
+slack.send(`Initializing newly filed project update task with ${intervalMin} min interval`);
 
-const triggerNewFiledProjectsUpdate = () => {
-  request({
-    method: 'GET',
-    uri: `${HOST}/projects/new-filed`,
-    timeout: 5 * 60 * 1000, // 5 minutes -- this can be slow, might get slower, don't want to have to come change this
-    simple: false,
-    resolveWithFullResponse: true,
-  })
-    .then((res) => {
-      const jsonBody = JSON.parse(res.body);
+// Parsing the response as a proxy for ensuring we are getting back an expected response
+const requestAndParse = async () => {
+  try {
+    const res = await fetch(`${HOST}/projects/new-filed`);
+    const json = await res.json();
+    return { ok: res.ok, json };
+  } catch (err) {
+    throw Error(err);
+  }
+};
 
-      let slackMessage = `Updated newly filed project geometries: ${jsonBody.success} successfully; ${jsonBody.failure} with failures`;
-      if (res.statusCode !== 200) {
-        slackMessage += `; ${jsonBody.error} with errors`;
-      }
+// add a retry to the request
+const requestWithRetry = async (retries) => {
+  try {
+    const res = await requestAndParse();
+    return res;
+  } catch (err) {
+    if (retries > 0) {
+      console.log('Retrying...'); //eslint-disable-line
+      return requestWithRetry(retries - 1);
+    }
+    console.log(err); //eslint-disable-line
+    throw Error(`Exceeded ${maxRetries} attempts`);
+  }
+};
 
-      const attachments = [{
-        text: `error messages: ${jsonBody.errorMessages}\nfailure messages: ${jsonBody.failureMessages}`,
-      }];
+const triggerNewFiledProjectsUpdate = async () => {
+  try {
+    const { ok, json } = await requestWithRetry(maxRetries);
+    let slackMessage = `Updated newly filed project geometries: ${json.success} successfully; ${json.failure} with failures`;
+    if (!ok) {
+      slackMessage += `; ${json.error} with errors`;
+    }
 
-      slack.send({ text: slackMessage, attachments });
-    })
-    .catch((err) => {
-      console.log(err); //eslint-disable-line
-      slack.send('ALERT: Unable to update newly filed project geometries');
-    });
+    const attachments = [{
+      text: `error messages: ${json.errorMessages}\nfailure messages: ${json.failureMessages}`,
+    }];
 
-  setTimeout(triggerNewFiledProjectsUpdate, interval_min * 60 /* sec */ * 1000 /* millisec */);
+    slack.send({ text: slackMessage, attachments });
+  } catch (err) {
+    console.log(err); //eslint-disable-line
+    slack.send('ALERT: Unable to update newly filed project geometries');
+  }
+
+  setTimeout(triggerNewFiledProjectsUpdate, intervalMin * 60 /* sec */ * 1000 /* millisec */);
 };
 
 triggerNewFiledProjectsUpdate();
