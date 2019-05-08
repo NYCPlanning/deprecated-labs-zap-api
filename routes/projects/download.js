f6853f9994bd4fdf9b18d809377ccd93b33db91b
julialucyhogan <hogan.julia@gmail.com>
2019-05-08 16:59:44 -0400

    Remove streaming from CSV download endpoint (#124)

--- routes/projects/download.js
+++ routes/projects/download.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const Json2csvTransform = require('json2csv').Transform;
-const QueryStream = require('pg-query-stream');
-const JSONStream = require('JSONStream');
+const { parse: json2csv } = require('json2csv');
 const buildProjectsSQL = require('../../utils/build-projects-sql');
 const transformActions = require('../../utils/transform-actions');
 
@@ -15,42 +13,24 @@ router.get('/', async (req, res) => {
 
   const SQL = buildProjectsSQL(query, 'download');
 
-  // see if this query has data
-  const { rowcount } = await app.db.one(`SELECT count(*) AS rowcount FROM (${SQL}) a`);
+  try {
+    const data = await app.db.any(SQL);
+    // only stream a response if there is data
+    if (data.length) {
+      data.map(row => transformActions(row));
+      const csv = json2csv(data, { highWaterMark: 16384, encoding: 'utf-8' });
 
-  // only stream a response if there is data
-  if (rowcount > 0) {
-    // you can also use pgp.as.format(query, values, options)
-    // to format queries properly, via pg-promise;
-    const qs = new QueryStream(SQL);
-
-    const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
-    const json2csv = new Json2csvTransform({}, transformOpts);
-
-    // Set approrpiate download headers
-    res.setHeader('Content-disposition', 'attachment; filename=projects.csv');
-    res.writeHead(200, { 'Content-Type': 'text/csv' });
-
-    // Flush the headers before we start pushing the CSV content
-    res.flushHeaders();
-
-    app.db.stream(qs, (s) => {
-      // initiate streaming into the console:
-      // objects are transformed, then serialized from JSON, then converted to CSV before being returned
-      s.pipe(transformActions).pipe(JSONStream.stringify()).pipe(json2csv).pipe(res);
-    })
-      .then((data) => {
-        console.log( // eslint-disable-line
-          'Total rows processed:', data.processed,
-          'Duration in milliseconds:', data.duration,
-        );
-      })
-      .catch((error) => {
-        console.log('ERROR:', error); // eslint-disable-line
+      res.setHeader('Content-type', 'text/csv');
+      res.send(csv);
+    } else {
+      res.status(500).send({
+        error: 'Unable to fetch data for CSV download',
       });
-  } else {
+    }
+  } catch (err) {
+    console.log(err); // eslint-disable-line
     res.status(500).send({
-      error: 'no data',
+      error: 'Unable to generate CSV from data',
     });
   }
 });
