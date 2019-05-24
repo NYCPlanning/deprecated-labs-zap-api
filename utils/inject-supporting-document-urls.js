9888a868a01d5279630e9811e2c6dd4a08adca90
trbmcginnis <taylormcgin@gmail.com>
2019-05-24 11:53:26 -0400

    if project has no action, do not inject supporting docs, caused ERROR

--- utils/inject-supporting-document-urls.js
+++ utils/inject-supporting-document-urls.js
@@ -14,52 +14,54 @@ const MILESTONE_TYPES = {
 
 async function injectSupportDocumentURLs(project) {
   // extract trimmed ULURP number
-  const ulurpNumbers = project.actions
-    .filter(({ dcp_ulurpnumber }) => dcp_ulurpnumber) // filter out nulls
-    .map(({ dcp_ulurpnumber }) => dcp_ulurpnumber.match(/[A-Z]?([A-Z0-9]{6,7})[A-Z]{3}/))
-    .map(ulurpNumber => (ulurpNumber || [])[1]);
+  if (project.actions) {
+    const ulurpNumbers = project.actions
+      .filter(({ dcp_ulurpnumber }) => dcp_ulurpnumber) // filter out nulls
+      .map(({ dcp_ulurpnumber }) => dcp_ulurpnumber.match(/[A-Z]?([A-Z0-9]{6,7})[A-Z]{3}/))
+      .map(ulurpNumber => (ulurpNumber || [])[1]);
 
-  // hit s3 object listing for all ULURP number & doc-type combination
-  const searchResults = await Promise.all([
-    ...ulurpNumbers
-      .map(ulurlp => fetch(`${S3_BUCKET_HOST}/?prefix=comments/${ulurlp}`)
-        .then(blob => blob.text())
-        .then(text => parseStringAsync(text))),
-    ...ulurpNumbers
-      .map(ulurlp => fetch(`${S3_BUCKET_HOST}/?prefix=letters-dob-hpd/${ulurlp}`)
-        .then(blob => blob.text())
-        .then(text => parseStringAsync(text))),
-  ]);
+    // hit s3 object listing for all ULURP number & doc-type combination
+    const searchResults = await Promise.all([
+      ...ulurpNumbers
+        .map(ulurlp => fetch(`${S3_BUCKET_HOST}/?prefix=comments/${ulurlp}`)
+          .then(blob => blob.text())
+          .then(text => parseStringAsync(text))),
+      ...ulurpNumbers
+        .map(ulurlp => fetch(`${S3_BUCKET_HOST}/?prefix=letters-dob-hpd/${ulurlp}`)
+          .then(blob => blob.text())
+          .then(text => parseStringAsync(text))),
+    ]);
 
-  // extract relevant contents, filter undefineds, and flatten
-  const allSupportingDocs = searchResults
-    .map(result => result['ListBucketResult']['Contents']) // eslint-disable-line
-    .filter(Boolean)
-    .reduce((acc, curr) => acc.concat(curr), []);
+    // extract relevant contents, filter undefineds, and flatten
+    const allSupportingDocs = searchResults
+      .map(result => result['ListBucketResult']['Contents']) // eslint-disable-line
+      .filter(Boolean)
+      .reduce((acc, curr) => acc.concat(curr), []);
 
-  project.milestones.forEach((milestone) => {
-    const { milestonename } = milestone;
-    const regex = MILESTONE_TYPES[milestonename];
+    project.milestones.forEach((milestone) => {
+      const { milestonename } = milestone;
+      const regex = MILESTONE_TYPES[milestonename];
 
-    if (regex) {
-      const foundDocuments = allSupportingDocs.filter(({ Key: key }) => {
-        const [filename] = key;
+      if (regex) {
+        const foundDocuments = allSupportingDocs.filter(({ Key: key }) => {
+          const [filename] = key;
 
-        return filename.match(regex);
-      });
-
-      if (foundDocuments.length) {
-        milestone.milestoneLinks = foundDocuments.map((doc) => {
-          const { Key: [filename] } = doc;
-
-          return {
-            filename: filename.split('/')[1],
-            url: `${S3_BUCKET_HOST}/${filename}`,
-          };
+          return filename.match(regex);
         });
+
+        if (foundDocuments.length) {
+          milestone.milestoneLinks = foundDocuments.map((doc) => {
+            const { Key: [filename] } = doc;
+
+            return {
+              filename: filename.split('/')[1],
+              url: `${S3_BUCKET_HOST}/${filename}`,
+            };
+          });
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 module.exports = injectSupportDocumentURLs;
