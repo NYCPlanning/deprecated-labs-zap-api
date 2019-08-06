8ecf71eb8221a82166e85555ee4c64e67c6e8a90
allthesignals <wilburnforce@gmail.com>
2019-08-06 11:38:01 -0400

    Re-integrate testing changes with crm & pg stubs

--- routes/feedback.js
+++ routes/feedback.js
@@ -13,30 +13,28 @@ const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECA
  * Creates a github issue for a given project
  */
 router.post('/', recaptcha.middleware.verify, async (req, res) => {
-
   if (req.recaptcha.error) {
     res.status(403).send({ status: 'Invalid captcha' });
   }
 
-  const { body: { projectid, projectname, text } }   = req.body;
-  ghrepo.issue({                                  
-    title: `Feedback about ${projectname}`,       
+  const { body: { projectid, projectname, text } } = req.body;
+  ghrepo.issue({
+    title: `Feedback about ${projectname}`,
     body: `Project ID: [${projectid}](https://zap.planning.nyc.gov/projects/${projectid})\nFeedback: ${text}`,
-  }, (error, { url }) => {                        
-
-    if (error) {                                  
+  }, (error, { url }) => {
+    if (error) {
       console.log('Error submitting feedback', error); // eslint-disable-line
-      res.status(500).send({                      
-        status: 'error',                          
-        error,                                    
-      });                                         
-    } else {                                      
-      res.status(201).send({                      
-        status: 'success',                        
-        url,                                      
-      });                                         
-    }                                             
-  });                                             
+      res.status(500).send({
+        status: 'error',
+        error,
+      });
+    } else {
+      res.status(201).send({
+        status: 'success',
+        url,
+      });
+    }
+  });
 });
 
 module.exports = router;
