b168176ae7bf0c942d75e7138dfae12cfee9a34b
allthesignals <wilburnforce@gmail.com>
2019-08-09 20:35:37 -0400

    Fix CRM contact lookup; include users lookup

--- queries/xml/filter-contacts-by-id.js
+++ queries/xml/filter-contacts-by-id.js
@@ -2,13 +2,18 @@
  * Returns FetchXML query param for fetching the contactid associated with a given email.
  * By default, the CRM does case-insensitive string matching
  */
-function contactIdXML(email) {
+function contactIdXML(contactid) {
   return [
     `<fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">`,
       `<entity name="contact">`,
+        `<attribute name="fullname" />`,
+        `<attribute name="telephone1" />`,
+        `<attribute name="emailaddress1" />`,
+        `<attribute name="parentcustomerid" />`,
+        `<attribute name="dcp_salutation" />`,
         `<attribute name="contactid" />`,
         `<filter type="and" >`,
-          `<condition attribute="emailaddress1" operator="eq" value="${email}" />`,
+          `<condition attribute="contactid" operator="eq" value="${contactid}" />`,
         `</filter>`,
       `</entity>`,
     `</fetch>`,
