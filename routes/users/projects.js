e7caabc8eaaff56684b5e6d2d7395b7847840b90
allthesignals <wilburnforce@gmail.com>
2019-08-15 11:28:40 -0400

    Cleanup authorized projects plus tests; Fix authorization logic
    # Conflicts:
    #	app.js
    #	queries/xml/user-projects.js
    #	routes/login.js
    #	routes/users/projects.js
    #	utils/session.js

---
+++ routes/users/projects.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const camelcase = require('camelcase');
+
+const UnauthError = require('../../errors/unauth');
+const BadRequestError = require('../../errors/bad-request');
+const userProjectsQueries = require('../../queries/xml/user-projects');
+const { isCurrentUser } = require('../../utils/session');
+const { flattenProjectRows } = require('../../utils/project/flatten-rows');
+
+const router = express.Router({ mergeParams: true });
+
+router.get('/', async (req, res) => {
+  const {
+    app: { crmClient },
+    params: { contactId },
+    query: { projectState = 'reviewed' },
+    session,
+  } = req;
+
+  try {
+    validateSession(session, contactId);
+    validateParams(projectState);
+
+    const targetQuery = userProjectsQueries[camelcase(projectState)](contactId);
+    const { value: projectRows } = await crmClient.doGet(`dcp_projects?fetchXml=${targetQuery}`);
+
+    const projects = projectRows.map(project => flattenProjectRows([project]));
+
+    res.send({
+      data: projects.map(project => ({
+        type: 'projects',
+        id: project.dcp_name,
+        attributes: project,
+      })),
+    });
+  } catch (e) {
+    if (e instanceof UnauthError) {
+      res.status(e.status).send({ errors: [{ code: e.code, detail: e.message }] });
+      return;
+    }
+
+    console.log(e);
+    res.status(500).send({ error: e.toString() });
+  }
+});
+
+// the middleware decodes the session into an object
+// if that fails, the session will be null
+function validateSession(session, contactId) {
+  if (!session) {
+    throw new UnauthError('Authentication required');
+  }
+
+  if (!isCurrentUser(contactId, session)) {
+    throw new UnauthError('Current user not authorized to access this route');
+  }
+}
+
+// ???
+function validateParams(filterType) {
+  const VALID_PROJECT_STATES = ['upcoming', 'to-review', 'reviewed', 'archived'];
+  if (!VALID_PROJECT_STATES.includes(filterType)) {
+    throw new BadRequestError(`filterType must be one of: ${VALID_PROJECT_STATES.join(', ')}`);
+  }
+}
+
+// See GH issue, must add another filter step
+// Removing for now
+// function getTargetMilestoneIds(userRole) ...
+
+module.exports = router;
