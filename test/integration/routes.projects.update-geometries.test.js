7281dd7e1548dfa19286e3ac4de98a38bd9ea5e0
allthesignals <wilburnforce@gmail.com>
2019-09-04 12:46:10 -0400

    Updates tests to pass

--- test/integration/routes.projects.update-geometries.test.js
+++ test/integration/routes.projects.update-geometries.test.js
@@ -8,12 +8,13 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const server = require('../../app');
-// const upsertGeoms = require('../../utils/upsert-geoms');
+
+const { USER_API_KEY } = process.env;
 
 describe('update-geometries route', () => {
   it('should respond with failure if id does not meet regex requirements', (done) => {
     chai.request(server)
-      .get('/projects/update-geometries/P201RQ0293')
+      .get(`/projects/update-geometries/P201RQ0293?API_KEY=${USER_API_KEY}`)
       .end((err, res) => {
         should.not.exist(err);
         res.status.should.equal(200);
@@ -27,12 +28,11 @@ describe('update-geometries route', () => {
 
   it('should respond failure message if project does not have BBLs', (done) => {
     chai.request(server)
-      .get('/projects/update-geometries/P1984Y0176')
+      .get(`/projects/update-geometries/P1984Y0176?API_KEY=${USER_API_KEY}`)
       .end((err, res) => {
         should.not.exist(err);
         res.status.should.equal(200);
         res.type.should.equal('application/json');
-
 
         // app response should include message
         res.body.response.message.should.equal('ZAP data does not list any BBLs for project P1984Y0176');
@@ -42,12 +42,11 @@ describe('update-geometries route', () => {
 
   it('should respond success message if project is updated', (done) => {
     chai.request(server)
-      .get('/projects/update-geometries/P2017M0085')
+      .get(`/projects/update-geometries/P2017M0085?API_KEY=${USER_API_KEY}`)
       .end((err, res) => {
         should.not.exist(err);
         res.status.should.equal(200);
         res.type.should.equal('application/json');
-
 
         // app response should include message
         res.body.response.message.should.equal('Updated geometries for project P2017M0085');
